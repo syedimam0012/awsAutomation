6b88d9fd68400432024870bf2252ccb823f9e6f3
syedimam0012 <syedimam0012@gmail.com>
2018-05-08 02:33:38 +0000

    Adding delete DB script

---
+++ rds/deleteDB.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import boto3
+
+db = sys.argv[1]
+rds = boto3.client('rds')
+
+try:
+    response = rds.delete_db_instance(
+        DBInstanceIdentifier=db,
+        SkipFinalSnapshot=True)
+    print resonse
+
+except Exception as error:
+    print error
